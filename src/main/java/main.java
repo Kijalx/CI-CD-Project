44a940cb0b86a7765eae4d5e3e84b9a8308c06d7
Aleksander Kijewski <G00396018@atu.ie>
2022-10-12 13:18:55 +0100

    Created Base project

---
+++ src/main/java/main.java
@@ -0,0 +1,13 @@
+/*
+    (Name of project)
+    CI/CD
+    G00391704: Sarah Mitchell
+    G00392003: Eoin Gallagher
+    G00396018: Aleksander Kijewski
+ */
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
