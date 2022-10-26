3b71c75e6e78661f1a9c4ce0f30d59af94ede196
Aleksander Kijewski <G00396018@atu.ie>
2022-10-26 16:08:06 +0100

    Setting up input stream

---
+++ src/main/java/GetCSVFile.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static java.lang.System.*;
+
+public class GetCSVFile {
+        public static void main(String[] args) throws IOException {
+            // loads a file from a resources folder
+            InputStream is = GetCSVFile.class.getClassLoader().getResourceAsStream("");
+
+    }
+}
