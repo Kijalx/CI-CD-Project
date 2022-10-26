01844f61f6051608b8ef3ca2070111bd909dc772
Michael agbo <Michael.e.agbo.work@gmail.com>
2022-10-26 16:24:51 +0100

    Added private variable

--- src/main/java/GetCSVFile.java
+++ src/main/java/GetCSVFile.java
@@ -5,7 +5,11 @@ import java.util.Collections;
 
 import static java.lang.System.*;
 
+
+
 public class GetCSVFile {
+    private int wages;
+
         public static void main(String[] args) throws IOException {
             // loads a file from a resources folder
             InputStream is = GetCSVFile.class.getClassLoader().getResourceAsStream("");
