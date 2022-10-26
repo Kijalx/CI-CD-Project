16b0a36fff6acb583531b78e9d94fd20f25e1fe3
Aleksander Kijewski <G00396018@atu.ie>
2022-10-26 16:55:05 +0100

    Added csv File reader

--- src/main/java/GetCSVFile.java
+++ src/main/java/GetCSVFile.java
@@ -1,14 +1,21 @@
+import org.apache.commons.io.IOUtils;
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
-
+import java.util.Scanner;
 import static java.lang.System.*;
 
 public class GetCSVFile {
-        public static void main(String[] args) throws IOException {
-            // loads a file from a resources folder
-            InputStream is = GetCSVFile.class.getClassLoader().getResourceAsStream("data.csv");
-
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(new File("src/main/resources/data.csv"));
+        //parsing a CSV file into the constructor of Scanner class
+        sc.useDelimiter(",");
+        //setting comma as delimiter pattern
+        while (sc.hasNext()) {
+            System.out.print(sc.next());
+        }
+        sc.close();
     }
 }
