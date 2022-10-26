d400e58be24bab4663ab6a4ad9a06d2101b03cb8
Michael agbo <Michael.e.agbo.work@gmail.com>
2022-10-26 16:56:48 +0100

    Added Gui window features *not finished yet*

--- src/main/java/GetCSVFile.java
+++ src/main/java/GetCSVFile.java
@@ -3,12 +3,33 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
 import static java.lang.System.*;
 
-public class GetCSVFile {
-        public static void main(String[] args) throws IOException {
+public class GetCSVFile extends JPanel {
+      //  public static void main(String[] args) throws IOException {
             // loads a file from a resources folder
-            InputStream is = GetCSVFile.class.getClassLoader().getResourceAsStream("data.csv");
+     //       InputStream is = GetCSVFile.class.getClassLoader().getResourceAsStream("data.csv");
 
-    }
+   // }
+    ArrayList<GetData> GetDataCompany = new ArrayList<>();
+    JFrame border = new JFrame("CSV Java Project");
+    JPanel button = new JPanel();
+
+    JButton load = new JButton("Load CSV");
+    JButton TotalHoursDisplay = new JButton("Display total amount of hours worked and sort csv in descending order.");
+    JButton ibanDisplay = new JButton("Display Rain Descending");
+    JButton bicDisplay = new JButton("Display Mean Wind Speed Descending");
+    JButton hoursDisplay = new JButton("Display Sun Ascending");
+    JButton Data = new JButton("Save CSV");
+    JTextArea Product = new JTextArea(40, 40);
+    JTextArea text = new JTextArea(1, 3);
+    int indicate = 0;
 }
