79d6fa0df7639270d1f679513f6207ee7c3cc1b9
Aleks Kijewski <G00396018@atu.ie>
2022-10-27 11:49:14 +0100

    Unfinished GetFilePath GUI

--- src/main/java/GetCSVFile.java
+++ src/main/java/GetCSVFile.java
@@ -1,15 +1,8 @@
-import org.apache.commons.io.IOUtils;
-
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.File;
 import java.util.Scanner;
-import static java.lang.System.*;
-
 public class GetCSVFile {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(new File("src/main/resources/data.csv"));
+        Scanner sc = new Scanner(new File("src/main/resources/Data.csv"));
         //parsing a CSV file into the constructor of Scanner class
         sc.useDelimiter(",");
         //setting comma as delimiter pattern
