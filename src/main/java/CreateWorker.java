68b608a4e767dbaeeebf1c5da54c79d09f84391c
Aleks <alekskijewski88@gmail.com>
2022-11-15 15:43:13 +0000

    Changed idea for project Created worker class and test

---
+++ src/main/java/CreateWorker.java
@@ -0,0 +1,5 @@
+public class CreateWorker {
+    public CreateWorker(String name, double iban, double bic, double hours, double rate, double overtimeHours, double totalPay, double normalPay, double overtimePay) {
+
+    }
+}
---
+++ src/test/java/CreateWorkerTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateWorkerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+    @Test
+    void testName(){
+
+    }
+    void testIban(){
+
+    }
+    @AfterEach
+    void tearDown() {
+    }
+}
