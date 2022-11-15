d695cf04159fc77adfb2f8d65b70de77f74c209f
Aleks <alekskijewski88@gmail.com>
2022-11-15 15:57:23 +0000

    Made Getters and setters for data and made all the tests

--- src/main/java/CreateWorker.java
+++ src/main/java/CreateWorker.java
@@ -1,5 +1,85 @@
 public class CreateWorker {
-    public CreateWorker(String name, double iban, double bic, double hours, double rate, double overtimeHours, double totalPay, double normalPay, double overtimePay) {
+    private String name;
+    private String iban;
+    private String bic;
+    private double hours;
+    private double rate;
+    private double overtimeHours;
+    private double totalPay;
+    private double normalPay;
+    private double overtimePay;
+    public CreateWorker(String name, String iban, String bic, double hours, double rate, double overtimeHours, double totalPay, double normalPay, double overtimePay) {
 
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public String getBic() {
+        return bic;
+    }
+
+    public void setBic(String bic) {
+        this.bic = bic;
+    }
+
+    public double getHours() {
+        return hours;
+    }
+
+    public void setHours(double hours) {
+        this.hours = hours;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public double getOvertimeHours() {
+        return overtimeHours;
+    }
+
+    public void setOvertimeHours(double overtimeHours) {
+        this.overtimeHours = overtimeHours;
+    }
+
+    public double getTotalPay() {
+        return totalPay;
+    }
+
+    public void setTotalPay(double totalPay) {
+        this.totalPay = totalPay;
+    }
+
+    public double getNormalPay() {
+        return normalPay;
+    }
+
+    public void setNormalPay(double normalPay) {
+        this.normalPay = normalPay;
+    }
+
+    public double getOvertimePay() {
+        return overtimePay;
+    }
+
+    public void setOvertimePay(double overtimePay) {
+        this.overtimePay = overtimePay;
     }
 }
--- src/test/java/CreateWorkerTest.java
+++ src/test/java/CreateWorkerTest.java
@@ -11,10 +11,91 @@ class CreateWorkerTest {
     void setUp() {
     }
     @Test
+    void testConstructor(){
+    }
+
+    @Test
     void testName(){
 
     }
+
+    @Test
     void testIban(){
+
+    }
+
+    @Test
+    void testIbanFail(){
+
+    }
+
+    @Test
+    void testBic(){
+
+    }
+
+    @Test
+    void testBicFail(){
+
+    }
+
+    @Test
+    void testHours(){
+
+    }
+
+    @Test
+    void testHoursFail(){
+
+    }
+
+    @Test
+    void testRate(){
+
+    }
+
+    @Test
+    void testRateFail(){
+
+    }
+
+    @Test
+    void testOvertime(){
+
+    }
+
+    @Test
+    void testOvertimeFail(){
+
+    }
+
+    @Test
+    void testTotalPay(){
+
+    }
+
+    @Test
+    void testTotalPayFail(){
+
+    }
+
+    @Test
+    void testNormalPay(){
+
+    }
+
+    @Test
+    void testNormalPayFail(){
+
+    }
+
+    @Test
+    void testOvertimePay(){
+
+    }
+
+    @Test
+    void testOvertimePayFail(){
 
     }
     @AfterEach
