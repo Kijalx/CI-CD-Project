faf08c35153a70ec70b1e4f6bf79f812b8f75ad8
Michael Agbo <michael.e.agbo.work@gmail.com>
2022-12-02 14:54:25 +0000

    Added Payslip Constructor

--- src/main/java/Payslip.java
+++ src/main/java/Payslip.java
@@ -7,4 +7,22 @@ public class Payslip {
     private double overtimePay;
     private double normalPay;
     private double totalPay;
+
+    private String iD;
+
+    public Payslip(String payslipName, String payslipIban, String payslipBic, double payslipRate, double payslipOvertime,
+                   double payslipOvertimePay, double payslipNormalPay, double payslipTotalPay, String payslipID) {
+
+            setWorkerDetails(payslipName);
+            setWorkerDetails(payslipIban);
+            setWorkerDetails(payslipBic);
+            setWorkerDetails(payslipIban);
+            setWorkerDetails(payslipRate);
+            setWorkerDetails(payslipOvertime);
+            setWorkerDetails(payslipOvertimePay);
+            setWorkerDetails(payslipNormalPay);
+            setWorkerDetails(payslipTotalPay);
+
+            setWorkerID(payslipID);
+    }
 }
