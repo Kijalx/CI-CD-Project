587cbf5162b1897e44792c87fae69b972b49275a
Michael Agbo <michael.e.agbo.work@gmail.com>
2022-12-02 14:08:10 +0000

    Added Payslip CLass Variables

--- src/main/java/Payslip.java
+++ src/main/java/Payslip.java
@@ -1,2 +1,10 @@
 public class Payslip {
+    private String name;
+    private String iban;
+    private String bic;
+    private double rate;
+    private double overtime;
+    private double overtimePay;
+    private double normalPay;
+    private double totalPay;
 }
