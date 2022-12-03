9d06c2606e85a301efc81ad5fe2a8a9b71338d7c
Michael Agbo <michael.e.agbo.work@gmail.com>
2022-12-03 13:33:46 +0000

    Created the employe controller class and imports

---
+++ src/main/java/EmployeeController.java
@@ -0,0 +1,14 @@
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotaion.GetMappping;
+import org.springframework.web.bind.annotaion.RestController;
+
+
+
+
+public class EmployeeController {
+
+
+}
