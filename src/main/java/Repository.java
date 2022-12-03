3870a215e1dc96a3796904c554ff90176cd0641a
Michael Agbo <michael.e.agbo.work@gmail.com>
2022-12-03 13:27:40 +0000

    added spring jpa finder by method

--- src/main/java/Repository.java
+++ src/main/java/Repository.java
@@ -1,8 +1,8 @@
 import Creation.CreateWorker;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface Repository extends CrudRepository<CreateWorker, Long>{
-
-
-
+    List<CreateWorker>findCreateWorkerByiD(int iD);
 }
